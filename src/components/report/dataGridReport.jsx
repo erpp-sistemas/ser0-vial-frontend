f3fdad536580defd87cf440e208b709c2b76d6ad
Carlos Adrian Martinez Flores <carlos.martinez@erpp.mx>
2025-01-15 09:30:27 -0600

    se muestran las fotos en el grid de registros

--- src/components/report/dataGridReport.jsx
+++ src/components/report/dataGridReport.jsx
@@ -1,27 +1,90 @@
 import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Download, Error } from "@mui/icons-material";
+import { DisabledByDefault, Download, Error } from "@mui/icons-material";
 import * as ExcelJS from "exceljs";
 import Loading from "../../components/modals/loading";
+import ImageViewer from "../viewer/imageViewer"; // Asegúrate de que este componente está importado correctamente.
 
 const DataGridReport = ({ data }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [noResults, setNoResults] = useState(false);
   const [showModalLoading, setShowModalLoading] = useState(false);
+  const [selectedPhotos, setSelectedPhotos] = useState([]);
+  const [openImageViewer, setOpenImageViewer] = useState(false); // Estado para controlar la visibilidad del visor de imágenes
 
   if (!data || data.length === 0) {
     return <p>No hay datos para mostrar</p>;
   }
 
   // Generar columnas dinámicamente
-  const columns = Object.keys(data[0]).map((key) => ({
-    field: key,
-    headerName: key.replace(/_/g, " ").toUpperCase(),
-    flex: 1,
-    minWidth: Math.max(100, key.length * 12), // Evitar que las columnas sean demasiado pequeñas
-  }));
+  const columns = Object.keys(data[0])
+    .filter((key) => key !== "photos")
+    .map((key) => ({
+      field: key,
+      headerName: key.replace(/_/g, " ").toUpperCase(),
+      flex: 1,
+      minWidth: Math.max(100, key.length * 12), // Evitar que las columnas sean demasiado pequeñas
+    }));
 
+  // Agregar columna para las fotos
+  columns.push({
+    field: "photos",
+    headerName: "FOTOS",
+    flex: 2,
+    minWidth: 150,
+    renderCell: (params) => {
+      const photos = params.value; // Array de fotos
+      if (!photos || photos.length === 0) {
+        return (
+          <div className="font-[sans-serif] flex flex-col gap-4 items-center mx-auto justify-center h-full">
+            <div className="flex gap-4 justify-center items-center">
+              <div className="flex items-center text-red-600 text-sm bg-red-50 px-3 py-1.5 tracking-wide rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-4 mr-2 fill-current"
+                  viewBox="0 0 24 24"
+                >
+                  <g fillRule="evenodd" clipRule="evenodd">
+                    <path
+                      d="M8.651 2.5c-2.52 0-4.15 1.729-4.15 4.404v8.146c0 2.676 1.63 4.404 4.15 4.404h8.647c2.525 0 4.156-1.728 4.156-4.404V6.904c.001-1.363-.415-2.501-1.203-3.29-.728-.729-1.747-1.114-2.949-1.114zm8.647 18.454H8.65C5.27 20.954 3 18.581 3 15.05V6.904C3 3.373 5.27 1 8.65 1h8.651c1.608 0 2.995.537 4.01 1.554 1.061 1.062 1.643 2.607 1.641 4.351v8.145c0 3.531-2.273 5.904-5.656 5.904z"
+                    />
+                    <path
+                      d="M9.856 6.69a1.096 1.096 0 1 0 .003 2.192 1.096 1.096 0 0 0-.003-2.193zm.001 3.69a2.598 2.598 0 0 1-2.596-2.595A2.598 2.598 0 0 1 9.857 5.19a2.6 2.6 0 0 1 2.597 2.595 2.599 2.599 0 0 1-2.597 2.596zM4.75 19.111a.75.75 0 0 1-.653-1.117c.06-.108 1.494-2.645 3.073-3.945 1.252-1.03 2.6-.464 3.686-.007.64.27 1.243.523 1.823.523.532 0 1.2-.94 1.79-1.769.818-1.156 1.748-2.464 3.11-2.464 2.17 0 4.043 1.936 5.05 2.976l.116.12a.751.751 0 0 1-.016 1.061.748.748 0 0 1-1.06-.016l-.118-.122c-.852-.88-2.438-2.519-3.972-2.519-.588 0-1.278.973-1.889 1.832-.838 1.18-1.705 2.401-3.01 2.401-.884 0-1.693-.34-2.406-.64-1.134-.479-1.648-.632-2.15-.218-1.365 1.124-2.707 3.498-2.72 3.521a.749.749 0 0 1-.655.383z"
+                    />
+                  </g>
+                </svg>
+                <span className="ml-1 text-xs">Sin fotos</span> {/* Leyenda a la derecha */}
+              </div>
+            </div>
+          </div>
+        );
+      }
+      return (
+        <div style={{ display: "flex", gap: "8px" }}>
+          {photos.slice(0, 3).map((photo, index) => (
+            <img
+              key={index}
+              src={photo.url} // Ajusta la propiedad según tu estructura de datos
+              alt={`Foto ${index + 1}`}
+              className="w-12 h-12 object-cover rounded-lg border border-gray-300 cursor-pointer"
+              onClick={() => {
+                setSelectedPhotos([photo]); // Establecer solo la foto seleccionada
+                setOpenImageViewer(true); // Abrir el modal de imagen
+              }}
+            />
+          ))}
+          {photos.length > 3 && (
+            <span style={{ color: "#888", fontSize: "12px" }}>
+              +{photos.length - 3} más
+            </span>
+          )}
+        </div>
+      );
+    },
+  });
+
+  // Función para manejar la descarga
   const handleDownload = async () => {
     try {
       setShowModalLoading(true);
@@ -142,7 +205,6 @@ const DataGridReport = ({ data }) => {
         <DataGrid
           rows={filteredUsers.length > 0 || searchTerm ? filteredUsers : data}
           columns={columns}
-          // getRowId={(row) => row.custom_id}
           getRowId={(row) => row.custom_id}
           disableSelectionOnClick
           className="text-sm
@@ -168,6 +230,15 @@ const DataGridReport = ({ data }) => {
           [&_.MuiSvgIcon-root]:dark:text-gray-200"
         />
       </div>
+
+      {/* Modal de visualización de imágenes */}
+      {openImageViewer && (
+        <ImageViewer
+          photos={selectedPhotos} // Asegúrate de pasar las fotos seleccionadas
+          open={openImageViewer}
+          onClose={() => setOpenImageViewer(false)} // Cerrar el modal
+        />
+      )}
     </div>
   );
 };
--- src/scenes/report/index.jsx
+++ src/scenes/report/index.jsx
@@ -119,25 +119,25 @@ export default function index() {
   const homogenizeDataJson = (data) => {
     // Paso 1: Parseamos data_json y extraemos los campos únicos
     const uniqueFields = new Set();
-
+  
     data.forEach((item) => {
       const parsedData = JSON.parse(item.data_json);
       Object.keys(parsedData).forEach((key) => uniqueFields.add(key));
     });
-
+  
     // Convertimos el Set a un arreglo
     const allFields = Array.from(uniqueFields);
-
+  
     // Paso 2: Homogenizamos los objetos
     const homogenizedData = data.map((item) => {
       const parsedData = JSON.parse(item.data_json);
-
+  
       // Creamos un objeto homogenizado basado en los campos únicos
       const homogenizedItem = {};
       allFields.forEach((field) => {
         homogenizedItem[field] = parsedData[field] || null; // Asigna null si el campo no existe
       });
-
+  
       // Formateamos la fecha del registro en UTC
       const registrationDate = new Date(item.registration_date);
       const formattedDate = `${registrationDate
@@ -160,31 +160,28 @@ export default function index() {
         .padStart(2, "0")} ${
         registrationDate.getUTCHours() < 12 ? "a.m." : "p.m."
       }`;
-
+  
       // Agregamos los campos adicionales del registro original
       return {
         custom_id: item.custom_id,
         fecha: formattedDate,
         registration_date_obj: registrationDate,
+        photos: item.photos || [], // Aseguramos que se agreguen las fotos al objeto
         ...homogenizedItem,
-        // place_id: item.place_id,
-        // form_id: item.form_id,
-        // latitude: item.latitude,
-        // longitude: item.longitude,
-        // user_id: item.user_id,
       };
     });
-
+  
     // Paso 3: Ordenamos los datos por fecha y hora en orden descendente
     homogenizedData.sort(
       (a, b) => b.registration_date_obj - a.registration_date_obj
     );
-
+  
     // Eliminamos el campo temporal usado para ordenamiento
     homogenizedData.forEach((item) => delete item.registration_date_obj);
-
+  
     return homogenizedData;
   };
+  
 
   return (
     <div className="p-6 font-[sans-serif]">
